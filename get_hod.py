35acd60e122a9e5ab82c1d836b458d8800b5eafb
Boryana <boryana.hadzhiyska@cfa.harvard.edu>
2023-06-20 23:05:46 -0400

    got rid of useless lines

--- get_hod.py
+++ get_hod.py
@@ -10,31 +10,15 @@ import matplotlib.pyplot as plt
 import plotparams
 plotparams.buba()
 
-# colors
-hexcolors_bright = ['#CC3311','#0077BB','#EE7733','#BBBBBB','#33BBEE','#EE3377','#0099BB']
-
 # simulation parameters
-tng_dir = "/mnt/alan1/boryanah/MTNG/";fp_dm = 'fp'
-#tng_dir = "/mnt/alan1/boryanah/MTNG/dm_arepo/";fp_dm = 'dm'
+tng_dir = "/mnt/alan1/boryanah/MTNG/"
+fp_dm = 'fp'
 #gal_types = ['LBG', 'LAE']
 gal_types = ['LAE']
 #n_gals = ['5.0e-04', '1.0e-03']
 n_gals = ['1.0e-02']
 snapshots = [129, 94]
 zs = [2., 3.]
-
-# plotting parameters
-left, width = 0.14, 0.85
-bottom, height = 0.1, 0.25
-spacing = 0.005
-rect_scatter = [left, bottom + (height + spacing), width, 0.6]
-rect_histx = [left, bottom, width, height]
-
-# start with a rectangular Figure
-plt.figure(figsize=(9, 10))
-ax_scatter = plt.axes(rect_scatter)
-#(direction='in', top=True, right=True)
-ax_histx = plt.axes(rect_histx)
 
 # loop over snapshots
 for i, snapshot in enumerate(snapshots):
@@ -44,7 +28,6 @@ for i, snapshot in enumerate(snapshots):
     print(z)
     
     # load other halo properties
-    #GroupPos_fp = np.load(tng_dir+f'data_{fp_dm}/GroupPos_{fp_dm}_{snapshot:d}.npy')
     GrMcrit_fp = np.load(tng_dir+f'data_{fp_dm}/Group_M_TopHat200_{fp_dm}_{snapshot:d}.npy')*1.e10
     SubhaloGrNr = np.load(tng_dir+f"data_{fp_dm}/SubhaloGroupNr_{fp_dm}_{snapshot:d}.npy")
 
@@ -83,7 +66,7 @@ for i, snapshot in enumerate(snapshots):
             count_cent_halo = np.zeros(len(GrMcrit_fp), dtype=int)
             count_cent_halo[grnr_cent_gal_uni] = cts
 
-            # save counts per halo
+            # save counts per halo (generally useful)
             want_save = True
             if want_save:
                 np.save(tng_dir+f"data_{fp_dm}/GroupCount{gal_type:s}_{n_gal:s}_{fp_dm}_{snapshot:d}.npy", count_halo)
@@ -97,35 +80,6 @@ for i, snapshot in enumerate(snapshots):
             count_sats_halo = count_halo - count_cent_halo
             print("satellite fraction", np.sum(count_sats_halo)/np.sum(count_halo))
 
-            # probability
-            want_probs = False
-            if want_probs:
-                # whether a halo has both a central and a satellite or just a satellite
-                choice_anysat = ((count_sats_halo > 0)).astype(int)
-                choice_acent = ((count_cent_halo == 1)).astype(int)
-                choice_anysat_acent = ((count_sats_halo > 0) & (count_cent_halo == 1)).astype(int)
-                choice_nosat_acent = ((count_sats_halo == 0) & (count_cent_halo == 1)).astype(int)
-                choice_anysat_nocent = ((count_sats_halo > 0) & (count_cent_halo == 0)).astype(int)
-                choice_nosat_nocent = ((count_sats_halo == 0) & (count_cent_halo == 0)).astype(int)
-
-                hist_anysat, _ = np.histogram(GrMcrit_fp, bins=mbins, weights=choice_anysat)
-                hist_acent, _ = np.histogram(GrMcrit_fp, bins=mbins, weights=choice_acent)
-                hist_anysat_acent, _ = np.histogram(GrMcrit_fp, bins=mbins, weights=choice_anysat_acent)
-                hist_nosat_acent, _ = np.histogram(GrMcrit_fp, bins=mbins, weights=choice_nosat_acent)
-                hist_anysat_nocent, _ = np.histogram(GrMcrit_fp, bins=mbins, weights=choice_anysat_nocent)
-                hist_nosat_nocent, _ = np.histogram(GrMcrit_fp, bins=mbins, weights=choice_nosat_nocent)
-                hist_norm, _ = np.histogram(GrMcrit_fp, bins=mbins)
-
-                # probability that a halo at a given mass bin holds blah
-                prob_anysat = hist_anysat/hist_norm
-                prob_acent = hist_acent/hist_norm
-                prob_anysat_acent = hist_anysat_acent/hist_norm
-                prob_nosat_acent = hist_nosat_acent/hist_norm
-                prob_anysat_nocent = hist_anysat_nocent/hist_norm
-                prob_nosat_nocent = hist_nosat_nocent/hist_norm
-
-                np.savez(f"data/{gal_type:s}_{n_gal:s}_{fp_dm}_{snapshot:d}.npz", mbinc=mbinc, prob_acent=prob_acent, prob_anysat=prob_anysat, prob_acent_given_anysat=prob_anysat_acent/prob_anysat, prob_anysat_given_acent=prob_anysat_acent/prob_acent)
-
             # histograms
             hist_gal, _ = np.histogram(GrMcrit_fp, bins=mbins, weights=count_halo)
             hist_cent_gal, _ = np.histogram(GrMcrit_fp, bins=mbins, weights=count_cent_halo)
@@ -134,10 +88,9 @@ for i, snapshot in enumerate(snapshots):
             hod_gal = hist_gal/hist_halo
             hod_sat_gal = hod_gal-hod_cent_gal
 
+            # std around the HOD mean (don't worrya bout this)
             std, _, _ = stats.binned_statistic(GrMcrit_fp, count_halo-count_cent_halo, statistic='std', bins=mbins)
-            #std, _, _ = stats.binned_statistic(GrMcrit_fp, count_sats_halo*(count_sats_halo-1), statistic='mean', bins=mbins); std = np.sqrt(std) # alternative definition
             poisson = np.sqrt(hod_sat_gal)
-            poiss_up, poiss_dw = hod_sat_gal + poisson, hod_sat_gal - poisson
             np.savez(f"data/hod_{n_gal}_{gal_type}_{snapshot}.npz", hod_cent_gal=hod_cent_gal, mbinc=mbinc, hod_sat_gal=hod_sat_gal, std=std, poisson=poisson)
             print("std = ", std)
             print("poisson = ", poisson)
