174218354f07a1fe19dc2abdd612f14153411b27
Boryana <boryana.hadzhiyska@cfa.harvard.edu>
2023-11-02 14:51:16 -0400

    plotting stuff

--- get_corr.py
+++ get_corr.py
@@ -1,19 +1,27 @@
 import numpy as np
 import Corrfunc
 import matplotlib.pyplot as plt
+from astropy.cosmology import FlatLambdaCDM
 
 # simulation parameters
 tng_dir = "/mnt/alan1/boryanah/MTNG/"
 fp_dm = 'fp'
-gal_type = 'LBG'
+gal_type = "LAE"#'LBG'
 n_gal = '1.0e-02'
-snapshot = 129
+#n_gal = '5.0e-04'
+snapshot = 129 #94 #129
 boxsize = 500.
-nthreads = 16
+nthreads = 32 #16
+if snapshot == 129: redshift = 2.
+elif snapshot == 94: redshift = 3.
 
 # load pos
 SubhaloPos = np.load(tng_dir+f"data_{fp_dm}/SubhaloPos_{fp_dm}_{snapshot:d}.npy")
+SubhaloVel = np.load(tng_dir+f"data_{fp_dm}/SubhaloVel_{fp_dm}_{snapshot:d}.npy")
+pos_parts = np.load(tng_dir+f"data_parts/pos_down_100000_snap_{snapshot:d}_{fp_dm}.npy")
+
 bins = np.logspace(-1, 1.4, 21)
+#bins = np.logspace(-1, 1.8, 21)
 binc = 0.5*(bins[1:] + bins[:-1])
 
 # load galaxy indices
@@ -24,10 +32,34 @@ else:
     
 # galaxy properties
 gal_pos = SubhaloPos[index]
+gal_vel = SubhaloVel[index]
+
+# set up cosmology
+h = 0.6774
+cosmo = FlatLambdaCDM(H0=h*100, Om0=0.3089, Tcmb0=2.725)
+H_z = cosmo.H(redshift).value
+print("H(z) = ", H_z)
+
+# adding RSDs
+pos_rsd = gal_pos.copy()
+pos_rsd[:, 2] += gal_vel[:, 2]*(1.+redshift)/H_z*h # Mpc/h
+pos_rsd %= boxsize
+np.save(f"data/{gal_type}_pos.npy", gal_pos)
+np.save(f"data/{gal_type}_pos_rsd.npy", pos_rsd)
+quit()
 
 # compute correlation function
 results = Corrfunc.theory.xi(boxsize, nthreads, bins, gal_pos[:, 0], gal_pos[:, 1], gal_pos[:, 2])
 Xi = results['xi']
+
+# compute correlation function
+results = Corrfunc.theory.xi(boxsize, nthreads, bins, pos_parts[:, 0], pos_parts[:, 1], pos_parts[:, 2])
+Xi_matter = results['xi']
+
+# plot results
+plt.figure(figsize=(9, 7))
+plt.plot(binc, np.sqrt(Xi/Xi_matter))
+plt.xscale('log')
 
 # plot results
 plt.figure(figsize=(9, 7))
